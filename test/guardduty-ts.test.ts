3d0450e87af165f00ba45623d42550eef03759f3
Bai <1074144@gmail.com>
2025-09-01 13:59:50 +0900

    Initial commit

---
+++ test/guardduty-ts.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GuarddutyTs from '../lib/guardduty-ts-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/guardduty-ts-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GuarddutyTs.GuarddutyTsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
