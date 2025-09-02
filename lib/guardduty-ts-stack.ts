13c2fa87bbef76d2e42938af310172ac72af138a
Bai <1074144@gmail.com>
2025-09-02 10:52:07 +0900

    test

--- lib/guardduty-ts-stack.ts
+++ lib/guardduty-ts-stack.ts
@@ -1,16 +1,41 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as guardduty from 'aws-cdk-lib/aws-guardduty';
 
-export class GuarddutyTsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class GuarddutyTsStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'GuarddutyTsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // 创建 GuardDuty 探测器（仅使用 features）
+    const detector = new guardduty.CfnDetector(this, 'GuardDutyDetector', {
+      enable: true,
+      findingPublishingFrequency: 'FIFTEEN_MINUTES',
+      features: [
+        {
+          name: 'EBS_MALWARE_PROTECTION',
+          status: 'ENABLED'
+        },
+        {
+          name: 'RDS_LOGIN_EVENTS',
+          status: 'ENABLED'
+        },
+        {
+          name: 'LAMBDA_NETWORK_LOGS',
+          status: 'ENABLED'
+        },
+        {
+          name: 'EKS_AUDIT_LOGS',
+          status: 'ENABLED'
+        },
+        {
+          name: 'S3_DATA_EVENTS',
+          status: 'ENABLED'
+        },
+        {
+          name: 'EKS_RUNTIME_MONITORING',
+          status: 'ENABLED'
+        }
+      ]
+    });
   }
 }
